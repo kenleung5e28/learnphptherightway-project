bc6559c1e7e7b74f8de4388144551065f851e412
Ken Leung <kenleung5e28@gmail.com>
2023-07-15 15:14:23 +0800

    WIP

--- app/App.php
+++ app/App.php
@@ -14,8 +14,14 @@ $transactions = array();
 
 $csv_file = fopen($csv_path, 'r');
 $header = fgetcsv($csv_file);
+foreach ($header as $column) {
+    $transactions[$column] = array();
+}
 while (($row = fgetcsv($csv_file)) !== false) {
-
+    foreach ($row as $i => $value) {
+        $column = $header[$i];
+        array_push($transactions[$column], $value);
+    }
 }
 fclose($csv_file);
 
