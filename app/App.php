efb258ff10d47fffa1ec8ff6983c9bc69b86865b
Ken Leung <kenleung5e28@gmail.com>
2023-07-15 12:47:59 +0800

    WIP

--- app/App.php
+++ app/App.php
@@ -3,4 +3,20 @@
 declare(strict_types = 1);
 
 // Your Code
-echo 'Hello, world!';
+$csv_path = FILES_PATH . 'sample_1.csv';
+
+if (!file_exists($csv_path)) {
+    echo 'Transaction file sample_1.csv does not exist';
+    return;
+}
+
+$transactions = array();
+
+$csv_file = fopen($csv_path, 'r');
+$header = fgetcsv($csv_file);
+while (($row = fgetcsv($csv_file)) !== false) {
+
+}
+fclose($csv_file);
+
+var_dump($transactions);
